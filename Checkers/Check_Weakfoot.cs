4cdfc9129d560a98c57d3c633a6bf44464abfe3f
DukeSilver4cc <dukesilver4cc@gmail.com>
2016-07-20 11:52:36 -0400

    Update Check_Weakfoot.cs
    Summer 2016 Rules Changes support Weak Foot Accuracy/Usage of LESS THAN (But not greater than) 2/2 and 4/4. Updated to > rather than != to support values less than 2/2 and 4/4.

--- Checkers/Check_Weakfoot.cs
+++ Checkers/Check_Weakfoot.cs
@@ -13,12 +13,12 @@ namespace AATF_15
             if (line.height <= constants.weakfoot_manlet_height)
             {
                 // Check manlet weak foot stats
-                if (line.Weak_Foot_Accuracy != constants.weakfoot_accuracy_manlet)
+                if (line.Weak_Foot_Accuracy > constants.weakfoot_accuracy_manlet)
                 {
                     Console.WriteLine(line.id + "\t" + line.name + " has invalid Weak Foot Accuracy (Is " + line.Weak_Foot_Accuracy + ", should be " + constants.weakfoot_accuracy_manlet + ")");
                     variables.errors++;
                 }
-                if (line.Weak_Foot_Usage != constants.weakfoot_usage_manlet)
+                if (line.Weak_Foot_Usage > constants.weakfoot_usage_manlet)
                 {
                     Console.WriteLine(line.id + "\t" + line.name + " has invalid Weak Foot Usage (Is " + line.Weak_Foot_Usage + ", should be " + constants.weakfoot_usage_manlet + ")");
                     variables.errors++;
@@ -27,12 +27,12 @@ namespace AATF_15
             else
             {
                 // Check regular weak foot stats
-                if (line.Weak_Foot_Accuracy != constants.weakfoot_accuracy)
+                if (line.Weak_Foot_Accuracy > constants.weakfoot_accuracy)
                 {
                     Console.WriteLine(line.id + "\t" + line.name + " has invalid Weak Foot Accuracy (Is " + line.Weak_Foot_Accuracy + ", should be " + constants.weakfoot_accuracy + ")");
                     variables.errors++;
                 }
-                if (line.Weak_Foot_Usage != constants.weakfoot_usage)
+                if (line.Weak_Foot_Usage > constants.weakfoot_usage)
                 {
                     Console.WriteLine(line.id + "\t" + line.name + " has invalid Weak Foot Usage (Is " + line.Weak_Foot_Usage + ", should be " + constants.weakfoot_usage + ")");
                     variables.errors++;
